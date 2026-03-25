88ffd93413e9c876758900d6facbaeef85b190ba
steeve mocto <amoct@students.kennesaw.edu>
2026-03-25 18:18:30 -0400

    yaml

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.get("/")
 
 
 def home():
-    
     return "CI/CD demo is running!"
+
 
 @app.get("/health")
 
 
 def health():
-
     return jsonify(status="ok")
 
 if __name__ == "__main__":
